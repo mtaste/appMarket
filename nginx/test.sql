5d2d771fa551a20dad7f262c7ab21cce4216d64e
ken <feng36201@126.com>
2017-02-23 12:09:20 +0800

    Signed-off-by: ken <feng36201@126.com>

--- nginx/test.sql
+++ nginx/test.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2017-02-22 11:52:54
+Date: 2017-02-23 12:08:56
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -39,23 +39,47 @@ CREATE TABLE `sys_auth` (
 -- Records of sys_auth
 -- ----------------------------
 INSERT INTO `sys_auth` VALUES ('02a18777e6774e5482488297fcf26653', '2', '新增', '2', 'auth/define/save.do', 'fa-plus', 'add', null, null, null, null, '1');
-INSERT INTO `sys_auth` VALUES ('1', '0', '平台管理', '1', null, null, null, null, null, null, null, null);
+INSERT INTO `sys_auth` VALUES ('1', '0', '平台管理', '1', '', '', '', null, null, null, null, '1');
 INSERT INTO `sys_auth` VALUES ('2', '1', '权限定义', '1', '/admin/mg/authDefine', '', '', null, null, null, null, '1');
 INSERT INTO `sys_auth` VALUES ('39b352a998a3499183295152e7e7e541', '1', '授权组织', '1', '/admin/mg/authOrg', '', '', null, null, null, null, '2');
+INSERT INTO `sys_auth` VALUES ('3a5d809b46484cce85a7a18fb0445df5', '479852e16c834956b8b6c3b801ffd11c', '删除', '2', 'org/dept/remove.do', 'fa-remove', 'remove', null, null, null, null, '3');
 INSERT INTO `sys_auth` VALUES ('3c856ca262be4c09afd1aa66f372039d', '39b352a998a3499183295152e7e7e541', '修改', '2', 'org/save.do', 'fa-edit', 'mod', null, null, null, null, '2');
 INSERT INTO `sys_auth` VALUES ('479852e16c834956b8b6c3b801ffd11c', 'f70012acb9f940bbb583fcd63e9af5ab', '组织管理', '1', '/admin/sys/orgDefine', null, null, null, null, null, null, null);
+INSERT INTO `sys_auth` VALUES ('4ac1d4f894704f45b779de16b723ec04', '479852e16c834956b8b6c3b801ffd11c', '修改', '2', 'org/dept/save.do', 'fa-edit', 'mod', null, null, null, null, '2');
 INSERT INTO `sys_auth` VALUES ('50c1cb37d53e4ea39da31b039b656364', 'e666e48e9d8b44a0a34ff136bcf2ad14', '客户模组', '1', '/admin/mg/org/list', null, null, null, null, null, null, null);
 INSERT INTO `sys_auth` VALUES ('67340c3f1b634546950bdb255e5982ac', 'a15ae14869f04822849571fb6ada622b', '用户管理', '1', '/admin/personnel/mgUser', null, null, null, null, null, null, null);
 INSERT INTO `sys_auth` VALUES ('67d5e55ab8cb4d5b93220645e6e85c99', '39b352a998a3499183295152e7e7e541', '权限保存', '2', 'org/auth/save', 'fa-save', 'save', null, null, null, null, '4');
 INSERT INTO `sys_auth` VALUES ('6db97da52d0d494fa8cc81c59329b179', '2', '删除', '2', 'auth/define/remove.do', 'fa-remove', 'remove', null, null, null, null, '3');
 INSERT INTO `sys_auth` VALUES ('76737c29c36a4d449b50a58137504355', 'e666e48e9d8b44a0a34ff136bcf2ad14', '机构登记', '1', '/admin/mg/org/register', null, null, null, null, null, null, null);
-INSERT INTO `sys_auth` VALUES ('a15ae14869f04822849571fb6ada622b', '0', '人事管理', '1', null, null, null, null, null, null, null, null);
+INSERT INTO `sys_auth` VALUES ('8fb8b57f988a49c39e7398f7e4a88f31', '479852e16c834956b8b6c3b801ffd11c', '新增', '2', 'org/dept/save.do', 'fa-plus', 'add', null, null, null, null, '1');
+INSERT INTO `sys_auth` VALUES ('a15ae14869f04822849571fb6ada622b', '0', '人事管理', '1', '', '', '', null, null, null, null, '3');
 INSERT INTO `sys_auth` VALUES ('aaf82f26159f426581e59355c7b610b1', '39b352a998a3499183295152e7e7e541', '删除', '2', 'org/remove.do', 'fa-remove', 'remove', null, null, null, null, '3');
 INSERT INTO `sys_auth` VALUES ('ad9d8b44b586456180a751fb87db8db2', '2', '保存', '2', 'auth/define/save.do', 'fa-mod', 'mod', null, null, null, null, '2');
+INSERT INTO `sys_auth` VALUES ('bbf82dfc30d54277a32d50189ce66030', '479852e16c834956b8b6c3b801ffd11c', '新增职务', '2', 'org/dept/role/save.do', 'fa-plus', 'addRole', null, null, null, null, '4');
 INSERT INTO `sys_auth` VALUES ('c9228a9899dc4f98b576a1f0e049e175', '39b352a998a3499183295152e7e7e541', '新增', '2', 'org/save.do', 'fa-plus', 'add', null, null, null, null, '1');
+INSERT INTO `sys_auth` VALUES ('d03715b26c894a59a99fc64351b2013a', '479852e16c834956b8b6c3b801ffd11c', '权限配置', '2', 'org/dept/role/save.do', 'fa-edit', 'modRole', null, null, null, null, '5');
 INSERT INTO `sys_auth` VALUES ('d3ecb27d914d427d95d4d8194b00d4ad', 'e666e48e9d8b44a0a34ff136bcf2ad14', '模组授权', '1', '/admin/mg/org/auth', null, null, null, null, null, null, null);
-INSERT INTO `sys_auth` VALUES ('e666e48e9d8b44a0a34ff136bcf2ad14', '0', '机构授权', '1', null, null, null, null, null, null, null, null);
-INSERT INTO `sys_auth` VALUES ('f70012acb9f940bbb583fcd63e9af5ab', '0', '系统管理', '1', '', null, null, null, null, null, null, null);
+INSERT INTO `sys_auth` VALUES ('e666e48e9d8b44a0a34ff136bcf2ad14', '0', '机构授权', '1', '', '', '', null, null, null, null, '4');
+INSERT INTO `sys_auth` VALUES ('f70012acb9f940bbb583fcd63e9af5ab', '0', '系统管理', '1', '', '', '', null, null, null, null, '2');
+INSERT INTO `sys_auth` VALUES ('f7adb4149aa0406d891fec5be6e151e3', '479852e16c834956b8b6c3b801ffd11c', '删除职务', '2', 'org/dept/role/remove.do', 'fa-remove', 'removeRole', null, null, null, null, '6');
+
+-- ----------------------------
+-- Table structure for `sys_dept`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_dept`;
+CREATE TABLE `sys_dept` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `parent_id` varchar(36) DEFAULT NULL,
+  `org_id` varchar(36) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of sys_dept
+-- ----------------------------
+INSERT INTO `sys_dept` VALUES ('4056026fd5d54543bacaed1f966b8bdc', 'JS部门', 'e2ff5e22cfe94054932ffca05e145934', 'e2ff5e22cfe94054932ffca05e145934');
+INSERT INTO `sys_dept` VALUES ('f3cba9023cd34bd1aa82b05d1aa5ddb2', 'JAVA部门', 'e2ff5e22cfe94054932ffca05e145934', 'e2ff5e22cfe94054932ffca05e145934');
 
 -- ----------------------------
 -- Table structure for `sys_dic`
@@ -107,10 +131,45 @@ CREATE TABLE `sys_org_auth` (
 -- ----------------------------
 -- Records of sys_org_auth
 -- ----------------------------
-INSERT INTO `sys_org_auth` VALUES ('bd042495-f8b1-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '02a18777e6774e5482488297fcf26653');
-INSERT INTO `sys_org_auth` VALUES ('bd042814-f8b1-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '2');
-INSERT INTO `sys_org_auth` VALUES ('bd042a6b-f8b1-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '6db97da52d0d494fa8cc81c59329b179');
-INSERT INTO `sys_org_auth` VALUES ('bd042baf-f8b1-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', 'ad9d8b44b586456180a751fb87db8db2');
+INSERT INTO `sys_org_auth` VALUES ('58139554-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '02a18777e6774e5482488297fcf26653');
+INSERT INTO `sys_org_auth` VALUES ('58139789-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '1');
+INSERT INTO `sys_org_auth` VALUES ('5813986e-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '2');
+INSERT INTO `sys_org_auth` VALUES ('58139951-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '39b352a998a3499183295152e7e7e541');
+INSERT INTO `sys_org_auth` VALUES ('58139aa0-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '3a5d809b46484cce85a7a18fb0445df5');
+INSERT INTO `sys_org_auth` VALUES ('58139b94-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '3c856ca262be4c09afd1aa66f372039d');
+INSERT INTO `sys_org_auth` VALUES ('58139c95-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '479852e16c834956b8b6c3b801ffd11c');
+INSERT INTO `sys_org_auth` VALUES ('58139dc1-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '4ac1d4f894704f45b779de16b723ec04');
+INSERT INTO `sys_org_auth` VALUES ('58139ec7-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '50c1cb37d53e4ea39da31b039b656364');
+INSERT INTO `sys_org_auth` VALUES ('58139fcb-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '67340c3f1b634546950bdb255e5982ac');
+INSERT INTO `sys_org_auth` VALUES ('5813a0bb-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '67d5e55ab8cb4d5b93220645e6e85c99');
+INSERT INTO `sys_org_auth` VALUES ('5813a180-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '6db97da52d0d494fa8cc81c59329b179');
+INSERT INTO `sys_org_auth` VALUES ('5813a296-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '76737c29c36a4d449b50a58137504355');
+INSERT INTO `sys_org_auth` VALUES ('5813a3b2-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', '8fb8b57f988a49c39e7398f7e4a88f31');
+INSERT INTO `sys_org_auth` VALUES ('5813a4a7-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', 'a15ae14869f04822849571fb6ada622b');
+INSERT INTO `sys_org_auth` VALUES ('5813a586-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', 'aaf82f26159f426581e59355c7b610b1');
+INSERT INTO `sys_org_auth` VALUES ('5813a647-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', 'ad9d8b44b586456180a751fb87db8db2');
+INSERT INTO `sys_org_auth` VALUES ('5813a71a-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', 'c9228a9899dc4f98b576a1f0e049e175');
+INSERT INTO `sys_org_auth` VALUES ('5813a82a-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', 'd3ecb27d914d427d95d4d8194b00d4ad');
+INSERT INTO `sys_org_auth` VALUES ('5813a92d-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', 'e666e48e9d8b44a0a34ff136bcf2ad14');
+INSERT INTO `sys_org_auth` VALUES ('5813aa1b-f970-11e6-a606-00163e028d22', 'e2ff5e22cfe94054932ffca05e145934', 'f70012acb9f940bbb583fcd63e9af5ab');
+
+-- ----------------------------
+-- Table structure for `sys_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `dept_id` varchar(36) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('4e8121e1b7784f208bf32bbcdc9627e9', '项目组长', 'f3cba9023cd34bd1aa82b05d1aa5ddb2');
+INSERT INTO `sys_role` VALUES ('e4dec7c2f5954e03984f90a6e03a59b4', '组长', '4056026fd5d54543bacaed1f966b8bdc');
+INSERT INTO `sys_role` VALUES ('fe242a8a14b848309e38a2653d5311b4', '项目经理', 'f3cba9023cd34bd1aa82b05d1aa5ddb2');
 
 -- ----------------------------
 -- Table structure for `sys_user`
@@ -128,7 +187,7 @@ CREATE TABLE `sys_user` (
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('admin', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'admin', '1');
+INSERT INTO `sys_user` VALUES ('admin', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'e2ff5e22cfe94054932ffca05e145934', '1');
 
 -- ----------------------------
 -- Function structure for `getSysAuthNodeID`
@@ -158,6 +217,48 @@ SELECT
 	group_concat(id) INTO sTempChd
 FROM
 	sys_auth a
+WHERE
+	FIND_IN_SET(a.parent_id, sTempChd) > 0;
+
+
+END
+WHILE;
+
+RETURN sTemp;
+
+
+END
+;;
+DELIMITER ;
+
+-- ----------------------------
+-- Function structure for `getSysDeptNodeId`
+-- ----------------------------
+DROP FUNCTION IF EXISTS `getSysDeptNodeId`;
+DELIMITER ;;
+CREATE DEFINER=`jst`@`%` FUNCTION `getSysDeptNodeId`(rootId VARCHAR(50)) RETURNS varchar(2000) CHARSET utf8mb4
+BEGIN
+	DECLARE
+		sTemp VARCHAR (2000);
+
+DECLARE
+	sTempChd VARCHAR (2000);
+
+
+SET sTemp = '$';
+
+
+SET sTempChd = rootId;
+
+
+WHILE sTempChd IS NOT NULL DO
+
+SET sTemp = concat(sTemp, ',', sTempChd);
+
+SELECT
+	group_concat(id) INTO sTempChd
+FROM
+	sys_dept a
 WHERE
 	FIND_IN_SET(a.parent_id, sTempChd) > 0;
 
